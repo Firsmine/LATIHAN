92f9dd3b9cc7461e06bfb18d6dc2ccbec093dae5
firsmine <sitishafira.fia@gmail.com>
2026-09-05 15:57:40 +0800

    1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request){
+        $request->validate([
+            'name'=>'required|string',
+            'email'=>'required|email|unique:users,email',
+            'password'=>'required|min:8|confirmed'
+        ]);
+
+        $user = User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>$request->password
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'success'=>true,
+            'message'=>'Registration Success',
+            'data'=>[
+                'user'=>$user,
+                'token'=>$token
+            ],
+        ], 201);
+    }
 }
