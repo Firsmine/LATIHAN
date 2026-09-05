3cd47da9b507849d0f659c1f4a00dfa94656b779
firsmine <sitishafira.fia@gmail.com>
2026-09-05 16:33:29 +0800

    1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -29,5 +30,32 @@ class UserController extends Controller
                 'token'=>$token
             ],
         ], 201);
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            'email'=>'required|string',
+            'password'=>'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || Hash::check($request->password, $user->password)){
+            return response()->json([
+                'success'=>false,
+                'message'=>'Login Failed. Email or Password is Wrong.'
+            ],401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'success'=>true,
+            'message'=>'Login success.',
+            'data'=>[
+                'user'=>$user,
+                'token'=>$token,
+            ]
+        ]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
+// use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -23,7 +23,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password'
-    ]
+    ];
 
     /**
      * Get the attributes that should be cast.
