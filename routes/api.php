896f83ceca2b0f34e343a533e6fe8f89c6750e3b
firsmine <sitishafira.fia@gmail.com>
2026-09-07 22:03:50 +0800

    1

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class,  'login']);
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/logout', [UserController::class, 'logout']);
+    Route::get('/me', [UserController::class, 'me']);
+});
