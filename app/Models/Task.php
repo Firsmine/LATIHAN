5aec0ca7d38fb611d522f69390311cedc0584b02
firsmine <sitishafira.fia@gmail.com>
2026-09-07 22:11:53 +0800

    1

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'status',
+        'deadline'
+    ];
 }
